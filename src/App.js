98a559621691479b3d5fea29dcc99a9b68c19726
tunamoon <113264059+tunamoon@users.noreply.github.com>
2023-03-16 23:55:19 -0400

    Initialize dependabot/npm_and_yarn/json5-1.0.2

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+  //handler goes here
+
+  render() {
+    return (
+      <div>
+        <p>Greetings!</p>
+        <p>Button goes here</p>
+      </div>
+    );
+  }
+}
