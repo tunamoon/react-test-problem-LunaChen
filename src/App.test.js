68f7b31dd5d38072391ccf82812c033735551912
tunamoon <113264059+tunamoon@users.noreply.github.com>
2023-03-16 23:55:14 -0400

    Initial commit

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+
+// import your story from App.stories.js
+
+
+// Add your tests here
+// See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
