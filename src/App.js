43ee0b897a8f22f7614c4401c56cbd0c4a82f811
tunamoon <113264059+tunamoon@users.noreply.github.com>
2023-03-17 00:21:41 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      
-    };
-  }
-
-  //handler goes here
-  onst App = () => {
+export default function App() {
   const [buttonText, setButtonText] = useState('click me');
 
   function handleClick() {
-    setButtonText('thanks');
+    if (buttonText === 'click me') {
+      setButtonText('thanks');
+    } else {
+      setButtonText('click me');
+    }
   }
-    
-    onst App = () => {
-  const [buttonText, setButtonText] = useState('thanks');
-
-  function handleClick() {
-    setButtonText('click me');
-  }
-      
 
   return (
     <div>
+      <p>Greetings!</p>
       <button onClick={handleClick}>{buttonText}</button>
     </div>
   );
-};
-
-
-
-
-  render() {
-    return (
-      <div>
-        <p>Greetings!</p>
-        <button type="button">click me</button>
-      </div>
-    );
-  }
 }
+
