d2145f5aad17a3b8bb91fd38827b77e37a1715ca
tunamoon <113264059+tunamoon@users.noreply.github.com>
2023-03-17 00:57:23 -0400

    Update App.stories.js

--- src/App.stories.js
+++ src/App.stories.js
@@ -1,2 +1,14 @@
 // Stories for testing `App` go here
 // See https://storybook.js.org/docs/react/writing-stories/introduction#how-to-write-stories
+import React from 'react'
+
+function Button(props) {
+  const { variant = 'primary', children, ...rest }
+  return (
+    <button className={'button ${variant}' {...rest}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
