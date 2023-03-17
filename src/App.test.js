e7046b7cfff2833a1948ae5a2866dd5e4b488bea
tunamoon <113264059+tunamoon@users.noreply.github.com>
2023-03-17 01:16:46 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,30 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 
 // import your story from App.stories.js
+// Form.test.js
 
+import { InvalidForm } from './App.test.js'; //
 
 // Add your tests here
+
+it('Checks if Button Initially displays "click me" ', () => {
+  const { getByTestId, getByText } = render(<InvalidForm {...InvalidForm.args} />);
+
+  getByText('click me');
+
+  const isFormValid = getByTestId('invalid-form');
+  expect(isFormValid).toBeInTheDocument();
+});
+
+it('Checks if clicking button changes its text to "thanks" ', () => {
+  const { getByTestId, getByText } = render(<InvalidForm {...InvalidForm.args} />);
+
+  fireEvent.click(getByText('Submit'));
+
+  const isFormValid = getByTestId('invalid-form');
+  expect(isFormValid).toBeInTheDocument();
+});
+
+
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
+
